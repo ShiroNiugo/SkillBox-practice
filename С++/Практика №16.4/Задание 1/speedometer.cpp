10c0323c33afdc0fcabf83a801986d7d217218f8
ShiroNiugo <kapustin754@gmail.com>
2022-09-18 14:19:07 +0300

    Practice 16.4 speedometer final?

--- С++/Практика №16.4/Задание 1/speedometer.cpp
+++ С++/Практика №16.4/Задание 1/speedometer.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
-bool compare(float speed_value, float border_speed, float delta)
-{
-	return (speed_value <= border_speed + delta) && (speed_value >= border_speed - delta);
-}
-
 int main()
 {
-	float speed_value = 0, delta = 0.01f;
+	float speedValue = 0;
 	char speed_str[100];
-	do{
+	do
+	{
+		float delta;
 		std::cout << "Input delta: ";
-		std::cin >> speed_value;
-		if(compare(speed_value, 150, delta)) continue;
-		std::sprintf(speed_str, "%.1f", speed_value);
-		std::cout << speed_str << std::endl;
-	} while (!compare(speed_value, 0, delta));
+		std::cin >> delta;
+		std::sprintf(speed_str, "%.01f", delta);
+		delta = std::stof(speed_str);
+
+		speedValue = (speedValue + delta <= 0) ? 0 : ((speedValue + delta >= 150) ? 150 : (speedValue + delta));
+		std::sprintf(speed_str, "%.1f", speedValue);
+		std::cout << speed_str << " km/h" << std::endl;
+		speedValue = std::stof(speed_str);
+	} while (speedValue > 0);
 	return 0;
 }
