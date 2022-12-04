4ec18d68bababaeff1a30948c65cc6b634ce19d6
ShiroNiugo <kapustin754@gmail.com>
2022-12-04 22:36:33 +0300

    Practice №25.4 Task 1

---
+++ С++/Практика №25.4/Задание 1/simulator of operations/include/command.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+
+char body[10][10];
+
+void fill()
+{
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			body[i][j] = '.';
+}
+
+void show()
+{
+	std::cout << "  ";
+	for (int i = 0; i < 10; i++)
+		std::cout << i << " ";
+	std::cout << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << i << " ";
+		for (int j = 0; j < 10; j++)
+			std::cout << body[i][j] << " ";
+		std::cout << std::endl;
+	}
+}
+
+void bresenhamAlgorithm(char ch, int x1, int y1, int x2, int y2)
+{
+	const int deltaX = abs(x2 - x1);
+	const int deltaY = abs(y2 - y1);
+	const int signX = x1 < x2 ? 1 : -1;
+	const int signY = y1 < y2 ? 1 : -1;
+	int error = deltaX - deltaY;
+	body[x2][y2] = ch;
+	while (x1 != x2 || y1 != y2)
+	{
+		body[x1][y1] = ch;
+		int error2 = error * 2;
+		if (error2 > -deltaY)
+		{
+			error -= deltaY;
+			x1 += signX;
+		}
+		if (error2 < deltaX)
+		{
+			error += deltaX;
+			y1 += signY;
+		}
+	}
+}
+
+void scalpel(int x1, int y1, int x2, int y2)
+{
+	bresenhamAlgorithm('s', x1, y1, x2, y2);
+	show();
+}
+
+void hemostat(int x, int y)
+{
+	body[x][y] = 'h';
+	show();
+}
+
+void tweezers(int x, int y)
+{
+	body[x][y] = 't';
+	show();
+}
+void suture(int x1, int y1, int x2, int y2)
+{
+	bresenhamAlgorithm('S', x1, y1, x2, y2);
+	show();
+}
---
+++ С++/Практика №25.4/Задание 1/simulator of operations/src/simulatorOfOperations.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include "../include/command.h"
+
+void addOneCoord(int& x, int& y)
+{
+	double coord;
+	do
+	{
+		printf("Input coordination (x,y): ");
+		std::cin >> coord;
+		x = (int)coord;
+		y = (coord - x + 0.0000001) * 10;
+	} while (body[x][y] != '.');
+}
+
+void addTwoCoord(int& x, int& y, int& x2, int& y2)
+{
+	double coord;
+	for (int i = 1; i <= 2;)
+	{
+		do
+		{
+			printf("Input %i coordination (x,y): ", i);
+			std::cin >> coord;
+			(i == 1 ? x : x2) = (int)coord;
+			(i == 1 ? y : y2) = (coord - (i == 1 ? x : x2) + 0.0000001) * 10;
+		} while (body[(i == 1 ? x : x2)][(i == 1 ? y : y2)] != '.');
+		i++;
+	}
+}
+
+int main()
+{
+	std::string command;
+	int x, y, x2, y2;
+	fill();
+	while (1)
+	{
+		std::cout << "Input command: ";
+		std::cin >> command;
+		if (command == "scalpel")
+		{
+			addTwoCoord(x, y, x2, y2);
+			scalpel(x, y, x2, y2);
+		}
+		else if (command == "hemostat")
+		{
+			addOneCoord(x, y);
+			hemostat(x, y);
+		}
+		else if (command == "tweezers")
+		{
+			addOneCoord(x, y);
+			tweezers(x, y);
+		}
+		else if (command == "suture")
+		{
+			addTwoCoord(x, y, x2, y2);
+			suture(x, y, x2, y2);
+			break;
+		}
+	}
+	return 0;
+}
