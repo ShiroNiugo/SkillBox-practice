7a58d330886d3804d6f0371d83a7459f168d8fff
ShiroNiugo <kapustin754@gmail.com>
2023-01-30 16:41:25 +0300

    Practice #32.5 Task 1

---
+++ С++/Практика №32.5/Задание 1/film/film.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+int main()
+{
+	std::ofstream file("../file.json");
+	nlohmann::json film 
+	= {
+		{"film","Dune: Part One"},
+		{"country", {"United States"}},
+		{"date", "2021"},
+		{"studio", {"Warner Bros. Pictures"}},
+		{"script author", {{"Jon Spaihts"},{"Denis Villeneuve"},{"Eric Roth"}}},
+		{"director", "Denis Villeneuve"},
+		{"producer", {{"Denis Villeneuve"},{"Mary Parent"},{"Cale Boyter"},{"Joe Caracciolo Jr."}}},
+		{"roles:", {
+					{"Paul Atreides", "Timothée Chalamet"},
+					{"Chani", "Zendaya"},
+					{"Lady Jessica Atreides", "Rebecca Ferguson"},
+					{"Duke Leto Atreides","Oscar Isaac"},
+					{"Duncan Idaho","Jason Momoa"}
+				   }}
+	};
+	file << film;
+	return 0;
+}
