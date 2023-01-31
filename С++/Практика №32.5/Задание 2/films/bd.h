794a3294562c59a5a572045bbfac9b7d67941fcc
ShiroNiugo <kapustin754@gmail.com>
2023-01-31 16:22:42 +0300

    Practice #32.5 Task 2 and 3

---
+++ С++/Практика №32.5/Задание 2/films/bd.h
@@ -0,0 +1,73 @@
+﻿void createBD() {
+	std::ofstream output("../file.json");
+	nlohmann::json filmsOutput
+		= {
+			{"Dune: Part One", {{"country", {"United States"}},
+								{"date", "2021"},
+								{"studio", {"Warner Bros. Pictures"}},
+								{"script author", {{"Jon Spaihts"},{"Denis Villeneuve"},{"Eric Roth"}}},
+								{"director", "Denis Villeneuve"},
+								{"producer", {{"Denis Villeneuve"},{"Mary Parent"},{"Cale Boyter"},{"Joe Caracciolo Jr."}}},
+								{"roles", {{"Paul Atreides", "Timothee Chalamet"},
+											{"Chani", "Zendaya"},
+											{"Lady Jessica Atreides", "Rebecca Ferguson"},
+											{"Duke Leto Atreides","Oscar Isaac"},
+											{"Duncan Idaho","Jason Momoa"}}
+								}}
+			},
+			{"Interstellar", {{"country", {{"United States"}, {"United Kingdom"}}},
+								{"date", "2014"},
+								{"studio", {{"Warner Bros. Pictures"},{"Paramount Pictures"}}},
+								{"script author", {{"Jonathan Nolan"},{"Christopher Nolan"}}},
+								{"director", "Christopher Nolan"},
+								{"producer", {{"Emma Thomas"},{"Christopher Nolan"},{"Lynda Obst"}}},
+								{"roles", {{"Cooper", "Matthew McConaughey"},
+											{"Brand", "Anne Hathaway"},
+											{"Murph", "Jessica Chastain"},
+											{"Romilly","David Gyasi"},
+											{"Professor Brand","Michael Caine"}}
+								}}
+			},
+			{"Gisaengchung", {{"country", {"South Korea"}},
+								{"date", "2019"},
+								{"studio", {"CJ Entertainment"}},
+								{"script author", {{"Bong Joon Ho"},{"Han Jin-won"}}},
+								{"director", "Bong Joon Ho"},
+								{"producer", {{"Kwak Sin-ae"},{"Moon Yang-kwon"},{"Bong Joon-ho"},{"Jang Young-hwan"}}},
+								{"roles", {{"Ki Taek", "Song Kang-ho"},
+											{"Dong Ik", "Lee Sun-kyun"},
+											{"Yeon Kyo", "Cho Yeo-jeong"},
+											{"Ki Woo","Choi Woo-sik"},
+											{"Ki Jung","Park So-dam"}}
+								}}
+			},
+			{"Inception", {{"country", {"United States"}},
+								{"date", "2010"},
+								{"studio", {"Warner Bros. Pictures"}},
+								{"script author", {{"Christopher Nolan"}}},
+								{"director", "Christopher Nolan"},
+								{"producer", {{"Emma Thomas"},{"Christopher Nolan"}}},
+								{"roles", {{"Cobb", "Leonardo DiCaprio"},
+											{"Arthur", "Joseph Gordon-Levitt"},
+											{"Ariadne", "Elliot Page"},
+											{"Eames","Tom Hardy"},
+											{"Saito","Ken Watanabe"}}
+								}}
+			},
+			{"The Imitation Game", {{"country", {"United States"}},
+								{"date", "2014"},
+								{"studio", "The Weinstein Company"},
+								{"script author", {{"Graham Moore"},{"Andrew Hodges"}}},
+								{"director", "Morten Tyldum"},
+								{"producer", {{"Nora Grossman"},{"Ido Ostrowsky"},{"Teddy Schwarzman"}}},
+								{"roles", {{"Alan Turing", "Benedict Cumberbatch"},
+											{"Joan Clarke", "Keira Knightley"},
+											{"Hugh Alexander", "Matthew Goode"},
+											{"Detective Robert Nock","Rory Kinnear"},
+											{"John Cairncross","Allen Leech"}}
+								}}
+			}
+	};
+	output << filmsOutput;
+	output.close();
+}
---
+++ С++/Практика №32.5/Задание 2/films/films.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <nlohmann/json.hpp>
+#include "bd.h"
+
+using namespace std;
+using json = nlohmann::json;
+
+void searchActors(json src, std::string name, std::vector<std::string> arr, std::string film = "") {
+	for (auto& [key, value] : src.items()) {
+		auto roles = value.find("roles");
+		if (roles != value.end())
+			searchActors(roles.value(), name, arr, key+": ");
+		else {
+			std::string n = (value.is_string()) ? value : "";
+			if (n.find(name) != std::string::npos)
+				arr.push_back(key);//std::cout << film << key << std::endl;
+		}
+	}
+	if(!arr.empty())
+		for(auto i : arr)
+			printf((i != arr.back() ? "%s, " : "%s\n"), (i == arr.front() ? (film + i).c_str() : i.c_str()));
+}
+
+int main()
+{
+	createBD();
+	std::ifstream input("../file.json");
+	if (input.is_open()) {
+		json filmsInput;
+		input >> filmsInput;
+		input.close();
+		std::string command;
+
+		while (1) {
+			std::cout << "Actor search (by first name or last name): ";
+			std::cin >> command;
+			if (command != "exit") { searchActors(filmsInput, command, std::vector<std::string>()); }
+			else { break; }
+		}
+	}
+	return 0;
+}
